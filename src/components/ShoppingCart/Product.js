ca72e87acc0ebc2530d6157d9427be47c33db705
andres delfino <andresldelfino@gmail.com>
2022-12-05 20:00:23 -0300

    agrego esqueleto carrito

---
+++ src/components/ShoppingCart/Product.js
@@ -0,0 +1,17 @@
+
+
+
+const Product = () => {
+
+
+
+  return (
+    <div> 
+        <h4> Name </h4>
+        <h5>$ Price</h5>
+        <button>Agregar</button>
+    </div>
+  )
+}
+
+export default Product
