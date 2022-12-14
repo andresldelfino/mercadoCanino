cf26e52e0227502125dc04510b2223cc15832083
YaniArruiz <shina_grafic@hotmail.com>
2022-12-14 17:56:00 -0300

    carousel, contact form, scroll

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from 'react';
+import './App.css';
+import CarouselComponent from "./components/carousel.component.jsx";
+import ContactForm from "./components/ContactForm.jsx";
+import ScrollButton from './components/ScrollButton.jsx';
+
 
 
 function App() {
+  return (
+    <div className="App">
+      <CarouselComponent />
+      <ContactForm />
+      <ScrollButton />
+     
+    </div>
   
+   
+     
+ 
+  );
 }
 
 export default App;
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      tipo1: ["Tipo-1"],
+     
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
