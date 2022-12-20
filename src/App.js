7d68293451100f2783baf619ca666e8e9f82fb6e
Rodrigo <rodrigo_gentilini@hotmail.com>
2022-12-19 22:34:22 -0300

    creo footer styles y agrego a app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ScrollButton from './components/ScrollButton.jsx';
 import CardList from "./components/CardList"
 import IMAGES from "./image/index"
 import Text from './components/Text';
+import Footer from './components/Footer';
 
 const products = [
   {id: 1, name: "Agility Adultos 20 KG", price: 5800, image:IMAGES.img1, alt:"Agility Adultos 20 KG"},
@@ -31,8 +32,11 @@ function App() {
       <CardList products={products}/>
       <Text/>
       <ContactForm />
+      <Footer/>
+      
       
     </div>
+    
   
    
      
