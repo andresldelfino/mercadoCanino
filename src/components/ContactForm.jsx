96ffee38bed1923658b04e8438d952b4306efab4
YaniArruiz <shina_grafic@hotmail.com>
2023-01-03 22:18:13 -0300

    implementación del Header

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -31,8 +31,8 @@ export const ContactForm = () => {
     
  
 
-  
-    <form ref={form} onSubmit={sendEmail} className="mt-6">
+  <div id='contacto'>
+    <form ref={form} onSubmit={sendEmail} className="mt-6" >
     
     <div className="relative flex flex-col justify-center min-h-screen overflow-hidden bg-gray-300 " >
       <div className=" w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-grey-600/40 ring-2 ring-green-600 lg:max-w-xl">
@@ -138,6 +138,7 @@ export const ContactForm = () => {
 
      
     </form>
+    </div>
   );
 };
 
