9becc42ade5292a89cc4775a4286101ca651b4f5
andres delfino <andresldelfino@gmail.com>
2023-01-10 21:57:36 -0300

    footer

--- src/components/ShoppingCart/ModalCartItem.js
+++ src/components/ShoppingCart/ModalCartItem.js
@@ -27,7 +27,9 @@ const ModalCartItem = ({ setModalOn, product, addToCart}) => {
     const {name, price} = product
     return (
 
-        <div className="bg-zinc-200 opacity-80 fixed inset-0 z-50">
+        <div className="bg-zinc-200 opacity-80  fixed inset-0 z-50">
+
+        
 
             <div className="flex h-screen justify-center items-center  ">
 
