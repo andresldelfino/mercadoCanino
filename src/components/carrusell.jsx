972ace1df30fe919a16186f3b3e7f0f17ab10ce8
Cocogomez10 <agussgomez@gmail.com>
2023-01-10 23:34:17 -0300

    bakcgroundPatasCarrusell

--- src/components/carrusell.jsx
+++ src/components/carrusell.jsx
@@ -3,34 +3,39 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 export default function CarouselComponent2() {
     return (
-        <div class="max-w-md p-1 mx-auto mt-3 bg-gray-200 sm:shadow-md sm::rounded-md sm:bg-gray-100 sm:p-1 md:bg-gray-200">
-            <Carousel infiniteLoop useKeyboardArrows autoPlay>
-                <div >
-                <img src= {'../img2/img1.png'} alt="integrante/yanina" />
-                </div>
-                <div>
-                <img src= {'../img2/img2.png'} alt="integrante/rodrigo" />
-                </div>
-                <div>
-                <img src= {'../img2/img3.png'} alt="integrante/noelia" />
-                </div>
-                <div>
-                <img src= {'../img2/img4.png'} alt="integrante/laura" />
-                </div>
-                <div>
-                <img src= {'../img2/img5.png'} alt="integrante/desiré" />
-                </div>
-                <div>
-                <img src= {'../img2/img6.png'} alt="integrante/agustin" />
-                </div>
-                <div>
-                <img src= {'../img2/img7.png'} alt="integrante/andres" />
-                </div>
-                <div>
-                <img src= {'../img2/img8.png'} alt="integrante/brandon" />
-                </div>
+        <div class="bg-[url('../public/img2/fondo.jpg')]">
+        <div class="  max-w-md p-1 mx-auto mt-0 bg-gray-200 sm:shadow-md sm::rounded-md sm:bg-gray-100 sm:p-1 md:bg-gray-200  ">
+        <Carousel infiniteLoop useKeyboardArrows autoPlay >
 
-            </Carousel>
-        </div>
+            <div >
+            <img src= {'../img2/img1.png'} alt="integrante/yanina" />
+            </div>
+            <div>
+            <img src= {'../img2/img2.png'} alt="integrante/rodrigo" />
+            </div>
+            <div>
+            <img src= {'../img2/img3.png'} alt="integrante/noelia" />
+            </div>
+            <div>
+            <img src= {'../img2/img4.png'} alt="integrante/laura" />
+            </div>
+            <div>
+            <img src= {'../img2/img5.png'} alt="integrante/desiré" />
+            </div>
+            <div>
+            <img src= {'../img2/img6.png'} alt="integrante/agustin" />
+            </div>
+            <div>
+            <img src= {'../img2/img7.png'} alt="integrante/andres" />
+            </div>
+            <div>
+            <img src= {'../img2/img8.png'} alt="integrante/brandon" />
+            </div>
+
+        </Carousel>
+    </div>
+
+    </div>
+
     );
 }
